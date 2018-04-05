3e0eba3faf131b0478f6795af9f5c3314e9f9384
yuwanyi3 <yuwanyi@jd.com>
2018-04-05 18:09:25 +0800

    framework demo

--- src/main/resources/static/js/my-app.js
+++ src/main/resources/static/js/my-app.js
@@ -10,38 +10,89 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
-// 下面代码是给“关于”页面使用的（关于页面加载完毕后触发）
-
-// 方式1：通过页面回调 (推荐):
-myApp.onPageInit('about', function (page) {
-    myApp.alert('"关于"页面加载完毕1!');
-})
-
-// 方式2：通过pageInit事件处理所有页面
-$$(document).on('pageInit', function (e) {
-    // 获取页面数据
-    var page = e.detail.page;
-
-    //判断是否是“关于”页面
-    if (page.name === 'about') {
-        myApp.alert('"关于"页面加载完毕2!');
-    }
-})
-
-// 方式2：通过pageInit事件处理所有页面(过滤出 data-page 属性为about的页面)
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    myApp.alert('"关于"页面加载完毕3!');
-})
-
-$$.get('http://localhost/get', {}, function(data) {
-    myApp.alert(data);
+myApp.onPageInit('view_detail', function (page) {
+    $$('.back').html("返回");
 });
 
-$$.ajax({
-    url:'http://localhost/get',
-    method:'GET',
-    dataType:'json',
-    success:function (result) {
-        myApp.alert(result.content);
-    }
+$$('.next-question').on('click', function () {
+    mainView.router.loadContent(
+        '<div class="pages navbar-through toolbar-through">' +
+        '  <!-- Page, data-page contains page name-->' +
+        '  <div class="page">' +
+        '    <!-- Scrollable page content-->' +
+        '    <div class="page-content">' +
+        '        <div class="paper-title">' +
+        '            2、教育技术应用到教育和教学领域，使教育的观念、内容、方法、手段和管理都发生了深刻变化，引起了人类思维方式和学习方式的变革。请指出下列选项中哪些是应用教育技术带来的教学方式和方法发生的变化？' +
+        '        </div>' +
+        '        <div class="list-block">' +
+        '            <ul>' +
+        '                <li>' +
+        '                    <label class="label-checkbox item-content">' +
+        '                        <input type="checkbox" name="my-checkbox" value="A">' +
+        '                        <div class="item-media">' +
+        '                            <i class="icon icon-form-checkbox"></i>' +
+        '                        </div>' +
+        '                        <div class="item-inner">' +
+        '                            <div class="item-title" style="white-space:normal;">' +
+        '                                A. 教师是教学过程的设计者、指导者； 教学媒体是学生学习的认知工具；以学生为主体' +
+        '                            </div>' +
+        '                        </div>' +
+        '                    </label>' +
+        '                </li>' +
+        '                <li>' +
+        '                    <label class="label-checkbox item-content">' +
+        '                        <input type="checkbox" name="my-checkbox" value="A">' +
+        '                        <div class="item-media">' +
+        '                            <i class="icon icon-form-checkbox"></i>' +
+        '                        </div>' +
+        '                        <div class="item-inner">' +
+        '                            <div class="item-title" style="white-space:normal;">' +
+        '                                A. 教师是教学过程的设计者、指导者； 教学媒体是学生学习的认知工具；以学生为主体' +
+        '                            </div>' +
+        '                        </div>' +
+        '                    </label>' +
+        '                </li>' +
+        '                <li>' +
+        '                    <label class="label-checkbox item-content">' +
+        '                        <input type="checkbox" name="my-checkbox" value="A">' +
+        '                        <div class="item-media">' +
+        '                            <i class="icon icon-form-checkbox"></i>' +
+        '                        </div>' +
+        '                        <div class="item-inner">' +
+        '                            <div class="item-title" style="white-space:normal;">' +
+        '                                A. 教师是教学过程的设计者、指导者； 教学媒体是学生学习的认知工具；以学生为主体' +
+        '                            </div>' +
+        '                        </div>' +
+        '                    </label>' +
+        '                </li>' +
+        '                <li>' +
+        '                    <label class="label-checkbox item-content">' +
+        '                        <input type="checkbox" name="my-checkbox" value="A">' +
+        '                        <div class="item-media">' +
+        '                            <i class="icon icon-form-checkbox"></i>' +
+        '                        </div>' +
+        '                        <div class="item-inner">' +
+        '                            <div class="item-title" style="white-space:normal;">' +
+        '                                A. 教师是教学过程的设计者、指导者； 教学媒体是学生学习的认知工具；以学生为主体' +
+        '                            </div>' +
+        '                        </div>' +
+        '                    </label>' +
+        '                </li>' +
+        '          </ul>' +
+        '      </div>' +
+        '      <div>' +
+        '          <a class="back" style="float:left;font-size:1.2rem;margin-left:10px;">上一题</a>' +
+        '          <a class="next-question">下一题</a>' +
+        '          <a href="view_detail.html" class="view-detail">查看解析</a>' +
+        '      </div>' +
+        '    </div>' +
+        '  </div>' +
+        '</div>')
 });
+// $$.ajax({
+//     url:'http://localhost/get',
+//     dataType:'json',
+//     success:function (result) {
+//         myApp.alert(result.content);
+//     }
+// });
